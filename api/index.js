4833d5962e6e6937f5de52c90b30555e1fe87f6a
Matheus Lino <matheuslino86a@gmail.com>
2023-02-25 12:30:18 -0400

    Remove db and general fixes

--- api/index.js
+++ api/index.js
@@ -12,18 +12,16 @@ var connection = mysql.createConnection({
 connection.connect();
 
 app.get("/", async (req, res) => {
-  let pessoas_lista = "<ul>";
-
   connection.query("SELECT * FROM pessoas", function (error, results, fields) {
-    if (error) throw error;
+    if (!results)
+      res.send(`<h1>Full Cycle Rocks!</h1><p>Lista de pessoas vazia!</p>`);
     const formated_results = JSON.parse(JSON.stringify(results));
-    console.log("formated_results", formated_results);
+
+    let pessoas_lista = "<ul>";
     for (let item of formated_results) {
       pessoas_lista += `<li>${item.id} - ${item.nome}</li>`;
     }
     pessoas_lista += "</ul>";
-
-    console.log("pessoas_lista 1", pessoas_lista);
 
     res.send(`<h1>Full Cycle Rocks!</h1>${pessoas_lista}`);
   });
